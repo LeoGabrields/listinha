0b1828bbdd9523e88ab4cdf9fd00def89e3f10d6
LeoGabrields <leogabrields97@gmail.com>
2023-02-12 12:05:05 -0300

    first

---
+++ app/lib/src/home/widgets/custom_drawer.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDrawer(
+      onDestinationSelected: (index) {
+        if (index == 1) {
+          Navigator.of(context).pushNamed('/config');
+        }
+      },
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 28, 16, 16),
+          child: Text(
+            'Opções',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ),
+        NavigationDrawerDestination(
+          icon: const Icon(Icons.sync),
+          label: Row(
+            children: [
+              const Text('Sincronizar'),
+              const SizedBox(width: 25),
+              Text(
+                '10/02/2023 às 21:04',
+                style: Theme.of(context).textTheme.bodySmall,
+              )
+            ],
+          ),
+        ),
+        const NavigationDrawerDestination(
+          icon: Icon(Icons.settings),
+          label: Text('Configurações'),
+        ),
+      ],
+    );
+  }
+}
---
+++ backend/bin/backend.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+import 'package:backend/backend.dart' as backend;
+
+
+void main(List<String> arguments) {
+  print('Hello world: ${backend.calculate()}!');
+  stdin.readLineSync();
+}
